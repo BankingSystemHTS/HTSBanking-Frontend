63fe16005f915fd1e37e9ee96fc2fce36eeb25c6
nathan <000savior001@gmail.com>
2025-05-15 13:08:48 -0700

    feat: left side bar nav link implemented

--- components/sidebar/LeftSideBar.tsx
+++ components/sidebar/LeftSideBar.tsx
@@ -8,51 +8,60 @@ import clsx from 'clsx'
 
 const LeftSideBar = ({ user }: SidebarProps) => {
    const pathname = usePathname()
-  return (
-     <section className="sidebar">
-        <nav className="flex flex-col gap-4">
-           <Link
-              href="/"
-              className="flex items-center mb-12 cursor-pointer
+   return (
+      <section className="sidebar">
+         <nav className="flex flex-col gap-4">
+            <Link
+               href="/"
+               className="flex items-center mb-12 cursor-pointer
               gap-2"
-           >
-              <Image
-                 src="icons/logo.svg"
-                 width={34}
-                 height={34}
-                 alt="logo"
-                 className="size-[24px] max-xl:size-14"
-              />
-              <h1 className="sidebar-logo">
-                 HTSBanking
-              </h1>
-           </Link>
-           {sidebarLinks.map((link) => {
-              return (
-                 <Link
-                    href={link.route}
-                    key={link.label}
-                    className={clsx(
-                       "flex items-center gap-2 rounded-lg p-2",
-                       pathname === link.route && "bg-gray-300 text-white"
-                    )}
-                 >
-                    <Image
-                       src={link.imgURL}
-                       width={24}
-                       height={24}
-                       alt="icon"
-                    />
-                    <span className="text-16 font-semibold text-gray-700">
-                       {link.label}
-                    </span>
-                    
-                 </Link>
-              )
-           })}
-        </nav>
-    </section>
-  )
+            >
+               <Image
+                  src="icons/logo.svg"
+                  width={34}
+                  height={34}
+                  alt="logo"
+                  className="size-[24px] max-xl:size-14"
+               />
+               <h1 className="sidebar-logo">
+                  HTSBanking
+               </h1>
+            </Link>
+            {sidebarLinks.map((link) => {
+               const isActive = pathname === link.route ||
+                  pathname.startsWith(`${link.route}/`)
+               return (
+                  <Link
+                     href={link.route}
+                     key={link.label}
+                     className={clsx(
+                        "sidebar-link",
+                        isActive && "bg-bank-gradient text-white",
+                     )}
+                  >
+                     <div className="relative size-6">
+                        <Image
+                           src={link.imgURL}
+                           width={24}
+                           height={24}
+                           alt="icon"
+                           className={clsx(isActive && "brightness-[3] invert-0")}
+                        />
+                     </div>
+                        <span className={clsx("text-16 font-semibold text-gray-700",
+                           isActive && "text-white"
+                        )}>
+                           {link.label}
+                        </span>
+
+                  </Link>
+               )
+            })}
+            User
+         </nav>
+         Footer1
+      </section>
+   )
 }
 
 export default LeftSideBar
